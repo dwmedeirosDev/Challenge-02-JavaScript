48c8968f283b9afb91a9d83a7adb1032db418a67
dwmedeiros94 <davidmedeiros94@hotmail.com>
2022-06-12 13:10:00 -0300

    Finalizando

--- script.js
+++ script.js
@@ -3,26 +3,21 @@
 //let idade = 18
 //if(idade>=18){
 //    console.log("Pode ingerir bebidas alcoólicas");
-//}
-//else{
+//}else{
 //    console.log("Proibido a venda de bebidas alcoólicas a menores")
 //}
 
 
 
-// Exercício 2 REVISAR
+// Exercício 2
 
-//let idade = 18
-//let epilepsiaFotossensivel = true;
-//if(idade >= 18){
-//    console.log("Liberado a entrada para maiores de 18 anos na Rave");
-//}
-//else if(epilepsiaFotossensivel){
-//    console.log("Proíbido a entrada na rave");
-//}
-//else{
-//    console.log("Entrada proibída");
-//}
+// let idade = 18
+// let epilepsiaFotossensivel = false;
+// if(idade > = 18 && epilepsiaFotossensivel == false ){
+//     console.log("Entrada liberada");
+// }else{
+//     console.log("Entrada negada");
+// }
 
 
 
@@ -31,20 +26,34 @@
 //let mes = "Março"
 //if((mes === "Janeiro") || (mes === "Dezembro")){
 //    console.log("Início ou final do ano");
-//}
-//else{
+//}else{
 //    console.log("Meio do ano");
 //}
 
 
 
-
-
 // Exercício bônus 1
+
+// let nome = "David"
+// let separaNome = nome.split("")
+// console.log(separaNome)
+// if (separaNome[0] == "R"){
+//     console.log("Esse nome começa com a letra R")
+// }else{
+//     console.log("Esse nome não começa com a letra R")
+// }
+
 
 
 // Exercício bônus 2
 
-
+let nome = "Fernanda Flores"
+let separaNome = nome.split (" ")
+console.log(separaNome)
+let contadorLetras = separaNome.length
+console.log (contadorLetras)
+if (contadorLetras[1] >= 6){
+    console.log("Teste")
+}
 
 
