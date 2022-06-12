d8fe789fd12285c51f1dc997b12ebc00bb22b806
dwmedeiros94 <davidmedeiros94@hotmail.com>
2022-06-12 10:12:51 -0300

    Pendente

---
+++ script.js
@@ -0,0 +1,50 @@
+// Exercício 1
+
+//let idade = 18
+//if(idade>=18){
+//    console.log("Pode ingerir bebidas alcoólicas");
+//}
+//else{
+//    console.log("Proibido a venda de bebidas alcoólicas a menores")
+//}
+
+
+
+// Exercício 2 REVISAR
+
+//let idade = 18
+//let epilepsiaFotossensivel = true;
+//if(idade >= 18){
+//    console.log("Liberado a entrada para maiores de 18 anos na Rave");
+//}
+//else if(epilepsiaFotossensivel){
+//    console.log("Proíbido a entrada na rave");
+//}
+//else{
+//    console.log("Entrada proibída");
+//}
+
+
+
+// Exercício 3
+
+//let mes = "Março"
+//if((mes === "Janeiro") || (mes === "Dezembro")){
+//    console.log("Início ou final do ano");
+//}
+//else{
+//    console.log("Meio do ano");
+//}
+
+
+
+
+
+// Exercício bônus 1
+
+
+// Exercício bônus 2
+
+
+
+
